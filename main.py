2b73302521628e39aaf3ef43791e6ccb3929ad9e
atharv1730 <atharvkumar06@gmail.com>
2025-02-01 14:28:44 -0500

    Setup

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import sys
 import pygame
+from constants import *
 
+# Pygame
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tic-Tac-Toe AI")
+
+
+def main():
+    pass
+
+
+main()
