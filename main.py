16ad6d26f122a7eb8bb1c86e1361b74c96c30059
atharv1730 <atharvkumar06@gmail.com>
2025-02-02 20:25:30 -0500

    Board class

--- main.py
+++ main.py
@@ -16,11 +16,28 @@ class Board:
         self.empty_squares = self.squares
         self.marked_squares = 0
 
+    def final_state(self):
+        pass
+
     def mark_square(self, row, col, player):
         self.squares[row][col] = player
 
     def is_square_empty(self, row, col):
         return self.squares[row][col] == 0
+    
+    def get_empty_squares(self):
+        empty_squares = []
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if self.is_square_empty(row, col):
+                    empty_squares.append((row, col))
+        return empty_squares
+
+    def is_full(self):
+        return self.marked_squares == 9
+    
+    def is_empty(self):
+        return self.marked_squares == 0
 
 class Game:
     def __init__(self):
